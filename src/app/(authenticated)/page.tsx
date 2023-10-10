41d50a587c5a7f027766e8e17c3568dafa54acdf
risqi2 <risqiikhsani12@gmail.com>
2023-10-10 11:30:36 +0700

    test

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,70 +1,91 @@
-import { CarCard } from '@/components/CarCard'
-import LeftNavBar from '@/components/left-navbar'
-import { Badge } from '@/components/ui/badge'
-import { Button, buttonVariants } from '@/components/ui/button'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Input } from "@/components/ui/input"
-import { Label } from '@/components/ui/label'
-import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/solid'
+import { CarCard } from "@/components/CarCard";
+import LeftNavBar from "@/components/left-navbar";
+import { Badge } from "@/components/ui/badge";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/solid";
 
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-import { Separator } from '@/components/ui/separator'
-
-
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Home() {
-    return (
-        <>
-                <div className='flex justify-around items-center my-5'>
+  return (
+    <>
+      <div className="flex justify-around items-center my-5">
+        <div className="flex gap-4">
+          <Input id="search" type="text" placeholder="search" />
+          <Button>Search</Button>
+        </div>
 
-                    <div className='flex gap-4'>
-                        <Input id="search" type="text" placeholder='search' />
-                        <Button>Search</Button>
-                    </div>
 
-                    <Button id='filter' className='rounded-3xl'>
-                        <AdjustmentsHorizontalIcon className="mr-2 h-4 w-4" />
-                        Filter
-                    </Button>
 
-                    <Select>
-                        <SelectTrigger className="w-[100px]" id="sort">
-                            <SelectValue placeholder="Sort" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectItem value="light">Light</SelectItem>
-                            <SelectItem value="dark">Dark</SelectItem>
-                            <SelectItem value="system">System</SelectItem>
-                        </SelectContent>
-                    </Select>
+        <Dialog>
+          <DialogTrigger className="p-1 border-solid border-2 flex rounded-md border-slate-200">
+            {/* <Button id="filter" className="rounded-3xl">
+              <AdjustmentsHorizontalIcon className="mr-2 h-4 w-4" />
+              Filter
+            </Button> */}
+            <AdjustmentsHorizontalIcon className="m-auto h-4 w-4 mr-2" />
+            Filter
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Filter</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
 
-                </div>
+        <Select>
+          <SelectTrigger className="w-[100px]" id="sort">
+            <SelectValue placeholder="Sort" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
 
-                <Separator className='my-6' />
+      <Separator className="my-6" />
 
-                <div className='flex flex-wrap gap-4 justify-center'>
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
-                </div>
-        </>
-    )
+      <div className="flex flex-wrap gap-4 justify-center">
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+      </div>
+    </>
+  );
 }
