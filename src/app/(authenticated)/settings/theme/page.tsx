25d4e01cf8999850c615f42d8058ed84c3853b22
risqi2 <risqiikhsani12@gmail.com>
2023-10-11 11:16:20 +0700

    test

--- src/app/(authenticated)/settings/theme/page.tsx
+++ src/app/(authenticated)/settings/theme/page.tsx
@@ -1,9 +1,18 @@
-import Title from "@/components/typography/Title";
+import { Separator } from "@/components/ui/separator"
+import AppearanceForm from "./_page/appearance-form"
 
-export default function Page(){
-    return(
-        <>
-            <Title title="Theme Settings"/>
-        </>
-    )
+export default function SettingsAppearancePage() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Appearance</h3>
+        <p className="text-sm text-muted-foreground">
+          Customize the appearance of the app. Automatically switch between day
+          and night themes.
+        </p>
+      </div>
+      <Separator />
+      <AppearanceForm />
+    </div>
+  )
 }
