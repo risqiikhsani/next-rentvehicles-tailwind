669c782ce35b09c3e7a176f7a89d95390739b13b
risqi <risqiikhsani16@gmail.com>
2023-10-12 21:09:01 +0700

    test

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,18 +3,17 @@ import Image from "next/image";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
-        
-            <div className="flex w-full">
-                <div>
-                    <Image src="/login1.jpg" width={640} height={960} alt="login" className="rounded-xl" />
-                </div>
 
-                <div className="flex items-center justify-center bg-slate-400 w-full">
-                    <div className="bg-slate-500 p-4">
+            <div className="flex w-full">
+                <Image src="/login1.jpg" width={640} height={960} alt="login" className="hidden lg:block rounded-l-2xl" />
+
+                <div className="flex items-center justify-center w-full lg:bg-gradient-to-r from-violet-500 to-fuchsia-500 lg:rounded-r-2xl">
+                    <div className="p-4 ">
                         {children}
                     </div>
                 </div>
             </div>
+
 
         </>
     )
--- src/components/typography/Title.tsx
+++ src/components/typography/Title.tsx
@@ -1,8 +1,10 @@
+import { Separator } from "../ui/separator";
+
 export default function Title({ title, text }: { title: string; text?: string }) {
     return (
       <>
       <div className="mb-10">
-      <h1 className="font-bold text-2xl">{title}</h1>
+      <h1 className="font-bold text-10xl">{title}</h1>
         {text && <h4>{text}</h4>}
       </div>
       </>
