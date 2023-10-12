931e1e226f92040a5bed57b8ffc5c8b0078e9b06
risqi2 <risqiikhsani12@gmail.com>
2023-10-12 14:23:05 +0700

    test

--- src/app/(authenticated)/posts/[slug]/page.tsx
+++ src/app/(authenticated)/posts/[slug]/page.tsx
@@ -1,7 +1,29 @@
-export default function Page(){
-    return(
-        <>
-        Post
-        </>
-    )
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="">
+          <Image src="/car1.png" alt="pic" width={500} height={500} />
+        </div>
+        <div className="col-span-2">
+          <h1 className="font-bold text-2xl">Lamborghini</h1>
+          <h3>Aventador</h3>
+          <div className="flex flex-col justify-end">
+            <div className="rounded-md my-1 p-1 bg-emerald-400">$50 / day</div>
+            <div className="rounded-md my-1 p-1 bg-emerald-400">
+              $300 / week
+            </div>
+            <div className="rounded-md my-1 p-1 bg-emerald-400">
+              $1100 / month
+            </div>
+          </div>
+          <div className="border-solid border-2 border-indigo-600 rounded-xl p-4 m-1">
+            <p>test</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
