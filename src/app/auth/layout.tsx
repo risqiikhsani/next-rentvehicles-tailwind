62242b6e667bdf9555d094af119e09e356d4aae5
risqi <risqiikhsani16@gmail.com>
2023-10-12 21:24:35 +0700

    test

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -7,8 +7,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <div className="flex w-full">
                 <Image src="/login1.jpg" width={640} height={960} alt="login" className="hidden lg:block rounded-l-2xl" />
 
-                <div className="flex items-center justify-center w-full lg:bg-gradient-to-r from-violet-500 to-fuchsia-500 lg:rounded-r-2xl">
-                    <div className="p-4 ">
+                <div className="flex mx-auto items-center justify-center h-screen w-max bg-[url('/login1.jpg')] lg:w-full lg:h-full lg:bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-2xl lg:rounded-r-2xl lg:rounded-l-none">
+                    <div className="p-4 opacity-90">
                         {children}
                     </div>
                 </div>
