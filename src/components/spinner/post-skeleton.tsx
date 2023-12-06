5665de9d83bc113a1f13b2caafe7a5ec6ad53258
risqi <risqiikhsani16@gmail.com>
2023-12-07 03:44:28 +0700

    test

--- src/components/spinner/post-skeleton.tsx
+++ src/components/spinner/post-skeleton.tsx
@@ -1,7 +1,7 @@
 export default function PostSkeleton() {
   return (
     <>
-      <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+      <div className="border border-blue-300 shadow rounded-md p-4 w-[300px]">
         <div className="animate-pulse flex space-x-4">
           <div className="rounded-full bg-slate-200 h-10 w-10"></div>
           <div className="flex-1 space-y-6 py-1">
